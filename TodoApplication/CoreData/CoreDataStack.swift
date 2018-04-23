28b54664fa55401072e9feb406cc65d72484b6a8
Regina Zhou <Matrix@Reginas-MacBook-Pro.local>
2018-04-22 22:14:21 -0400

    Achieved basic functions of a Todo app

---
+++ TodoApplication/CoreData/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  Todo_APP
+//
+//  Created by Borui Zhou on 2018-03-14.
+//  Copyright © 2018 Borui Zhou. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    var container: NSPersistentContainer {
+        let container = NSPersistentContainer(name: "Todos")
+        container.loadPersistentStores { (description, error) in
+            guard error == nil else {
+                print("Error: \(error!)")
+                return
+            }
+        }
+        
+        return container
+    }
+
+    var managedContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
---
+++ TodoApplication/ViewControllers/TodoTableViewController.swift
@@ -0,0 +1,181 @@
+//
+//  TodoTableViewController.swift
+//  Todo_APP
+//
+//  Created by Borui Zhou on 2018-03-14.
+//  Copyright © 2018 Borui Zhou. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TodoTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    var resultsController: NSFetchedResultsController<Todo>!
+    let coreDataStack = CoreDataStack()
+    static let greenyBlueColor = UIColor(red: 144.0/255.0, green:238.0/255.0, blue: 144.0/255.0, alpha: 1.0)
+    static let grey = UIColor(red: 94.0/255.0, green:94.0/255.0, blue: 94.0/255.0, alpha: 1.0)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let request: NSFetchRequest<Todo> = Todo.fetchRequest()
+        let sortDescriptors = NSSortDescriptor(key: "dateCreated", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptors]
+        resultsController = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: coreDataStack.managedContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        resultsController.delegate = self
+        
+        do {
+            try resultsController.performFetch()
+        } catch {
+            print("Perform fetch error: \(error)")
+        }
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsController.sections?[section].numberOfObjects ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+
+        let todo = resultsController.object(at: indexPath)
+        cell.textLabel?.text = todo.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = formatter.string(from: todo.dueDate!)
+        cell.detailTextLabel?.text = "Due: " + dateString
+        if todo.dueDate! < Date() {
+            cell.detailTextLabel?.textColor = .red
+        } else {
+            cell.detailTextLabel?.textColor = .black
+        }
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+            
+            let todo = self.resultsController.object(at: indexPath)
+            self.resultsController.managedObjectContext.delete(todo)
+            do {
+                try self.resultsController.managedObjectContext.save()
+                completion(true)
+            } catch {
+                print("delete failed: \(error)")
+                completion(false)
+            }
+        }
+        action.image = #imageLiteral(resourceName: "trash")
+        action.backgroundColor = .red
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Check") { (action, view, completion) in
+            let todo = self.resultsController.object(at: indexPath)
+            self.resultsController.managedObjectContext.delete(todo)
+            do {
+                try self.resultsController.managedObjectContext.save()
+                completion(true)
+            } catch {
+                print("delete failed: \(error)")
+                completion(false)
+            }
+        }
+        action.image = #imageLiteral(resourceName: "check")
+        action.backgroundColor = TodoTableViewController.greenyBlueColor
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowAddTodo", sender: tableView.cellForRow(at: indexPath))
+    }
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let _ = sender as? UIBarButtonItem, let vc = segue.destination as? AddTodoViewController {
+            vc.managedContext = resultsController.managedObjectContext
+        }
+        
+        if let cell = sender as? UITableViewCell, let vc = segue.destination as? AddTodoViewController {
+            vc.managedContext = resultsController.managedObjectContext
+            if let indexPath = tableView.indexPath(for: cell) {
+                let todo = resultsController.object(at: indexPath)
+                vc.todo = todo
+            }
+        }
+     }
+}
+
+extension TodoTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .automatic)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        case .update:
+            if let indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) {
+                let todo = resultsController.object(at: indexPath)
+                cell.textLabel?.text = todo.title
+            }
+        default:
+            break
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
